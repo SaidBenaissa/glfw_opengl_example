6fca2ecddb5063ff21d890a7e2e75b4493339715
Saïd Benaissa <d.sbenaissa@gmail.com>
2023-03-23 14:33:43 +0100

    glfw opengl app still has an issue (failed to create GLFW window).

---
+++ src/main.cpp
@@ -0,0 +1,62 @@
+#define GL_SILENCE_DEPRECATION
+#include <OpenGL/gl.h>
+
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+int main() {
+    // Initialize GLFW
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW\n";
+        return -1;
+    }
+
+    // Set OpenGL version to 3.3
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Create GLFW window
+    GLFWwindow* window = glfwCreateWindow(800, 600, "GLFW Example", nullptr, nullptr);
+    if (!window) {
+        std::cerr << "Failed to create GLFW window\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    // Make the window context current
+    glfwMakeContextCurrent(window);
+
+    // Set the framebuffer size callback
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // Main loop
+    while (!glfwWindowShouldClose(window)) {
+        // Clear the screen
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Draw a triangle
+        glBegin(GL_TRIANGLES);
+        glColor3f(1.0f, 0.0f, 0.0f);
+        glVertex3f(-0.5f, -0.5f, 0.0f);
+        glColor3f(0.0f, 1.0f, 0.0f);
+        glVertex3f(0.5f, -0.5f, 0.0f);
+        glColor3f(0.0f, 0.0f, 1.0f);
+        glVertex3f(0.0f, 0.5f, 0.0f);
+        glEnd();
+
+        // Swap buffers
+        glfwSwapBuffers(window);
+
+        // Poll for events
+        glfwPollEvents();
+    }
+
+    // Clean up
+    glfwTerminate();
+    return 0;
+}
